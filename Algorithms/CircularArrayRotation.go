c88245ad57dcad7a18208a883889b17d3ccf3b9d
Michael Van Brummen <michaelvanbrummen@gmail.com>
2016-08-21 13:45:30 +1000

    Optimising circluar array

--- Algorithms/CircularArrayRotation.go
+++ Algorithms/CircularArrayRotation.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func rotate(ints []int, k int) {
+	l := len(ints)
+	k = k % l
+	if k == 0 {
+		return
+	}
+	tmp := make([]int, l)
+	for i := 0; i < l; i++ {
+		for i+k >= l {
+			k -= l
+		}
+		tmp[i+k] = ints[i]
+	}
+	copy(ints, tmp)
+}
+
 func main() {
 	var n, k, q int
 	fmt.Scanf("%d %d %d", &n, &k, &q)
@@ -13,9 +29,7 @@ func main() {
 	for i := range queries {
 		fmt.Scanf("%d", &queries[i])
 	}
-	for i := 0; i < k; i++ {
-		ints = append(ints[n-1:], ints[:n-1]...)
-	}
+	rotate(ints, k)
 	for _, query := range queries {
 		fmt.Printf("%d\n", ints[query])
 	}
