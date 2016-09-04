4014f48daa3adcdea6e66be140b1f56a3862ee92
Michael Van Brummen <michaelvanbrummen@gmail.com>
2016-09-04 10:35:00 +1000

    Editing kangaroo

--- Algorithms/Kangaroo.go
+++ Algorithms/Kangaroo.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	var x1, v1, x2, v2 int
 	fmt.Scanf("%d %d %d %d", &x1, &v1, &x2, &v2)
-	if v1-v2 == 0 {
+	if x2 > x1 && v2 > v1 {
+		fmt.Println("NO")
+	} else if v1-v2 == 0 {
 		fmt.Println("NO")
 	} else {
 		n := (x2 - x1) % (v1 - v2)
