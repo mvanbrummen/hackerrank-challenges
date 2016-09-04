d554c03f7faaadf8ba13b5af47d4ca743609c07a
Michael Van Brummen <michaelvanbrummen@gmail.com>
2016-09-04 10:29:33 +1000

    Added kangaroo

---
+++ Algorithms/Kangaroo.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x1, v1, x2, v2 int
+	fmt.Scanf("%d %d %d %d", &x1, &v1, &x2, &v2)
+	if v1-v2 == 0 {
+		fmt.Println("NO")
+	} else {
+		n := (x2 - x1) % (v1 - v2)
+		if n == 0 {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+	}
+}
