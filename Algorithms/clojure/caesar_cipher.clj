1f017e9413daf28be5e97a1085a8ce77df96116b
Michael Van Brummen <michaelvanbrummen@gmail.com>
2019-09-15 17:22:08 +1000

    Finished caesar cipher

--- Algorithms/clojure/caesar_cipher.clj
+++ Algorithms/clojure/caesar_cipher.clj
@@ -1,22 +1,29 @@
+(def alphabet-total 26)
 
+(defn adjust-k [c k]
+  (let [k (+ k (int c))]
+    (if (or (and (Character/isLowerCase c) (> k (int \z)))
+            (and (Character/isUpperCase c) (> k (int \Z))))
+      (- k alphabet-total)
+      k)))
 
 ; Complete the caesarCipher function below.
+
 (defn caesarCipher [s k]
   (apply str (map #(char (Integer. %))
                   (map (fn [c]
                          (if (Character/isLetter c)
-                           (+ k (int c))
+                           (adjust-k c k)
                            (int c))) (char-array s)))))
+                           
+(def fptr (get (System/getenv) "OUTPUT_PATH"))
 
-    ; (def fptr (get (System/getenv) "OUTPUT_PATH"))
+(def n (Integer/parseInt (clojure.string/trim (read-line))))
 
-    ; (def n (Integer/parseInt (clojure.string/trim (read-line))))
+(def s (read-line))
 
+(def k (Integer/parseInt (clojure.string/trim (read-line))))
 
-(def s "middle-Outz")
+(def result (caesarCipher s k))
 
-(def k 2)
-
-(println (caesarCipher s k))
-
-    ; (spit fptr (str result "\n") :append true)
+(spit fptr (str result "\n") :append true)
